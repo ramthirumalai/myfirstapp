055b3eaa8fa7c388f65952f4abb4adb4e8f31a52
Ramakrishnan Thirumalai <ramakrishnan.thirumalai@uniphore.com>
2025-02-23 07:13:49 -0800

    initial project structure

--- backend/src/main/java/com/example/backend/repository/UserRepository.java
+++ backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -1,4 +1,7 @@
-package.data.jpa.repository.JpaRepository;
+package com.example.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.backend.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 }
