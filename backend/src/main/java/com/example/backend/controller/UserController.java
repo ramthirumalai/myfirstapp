1677bc08feca44fcc9239645942bdbee30afbbdd
Ramakrishnan Thirumalai <ramakrishnan.thirumalai@uniphore.com>
2025-02-23 19:09:05 -0800

    initial project structure

--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -1,23 +1,21 @@
 package com.example.backend.controller;
 
-import com.example.backend.model.User;
-import com.example.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.backend.repository.UserRepository;
+import com.example.backend.model.User;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
 public class UserController {
 
     @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/users")
     public List<User> getAllUsers() {
         return userRepository.findAll();
-    }
-
-    @PostMapping
-    public User createUser(@RequestBody User user) {
-        return userRepository.save(user);
     }
 }
